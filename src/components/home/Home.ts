444431666c9ba6bf74236ff05e3439c67a7e0310
Andy Lindsay <andydlindsay@gmail.com>
2019-01-27 16:14:38 -0800

    adds player movement bound to wasd

--- src/components/home/Home.ts
+++ src/components/home/Home.ts
@@ -1,4 +1,6 @@
-import { Component, Vue } from 'vue-property-decorator';
+// import { Vue } from 'vue-property-decorator';
+import Vue from 'vue';
+import Component from 'vue-class-component';
 import Node from '@/components/node/Node.ts'; // @ is an alias to /src
 import WithRender from './home.html';
 
@@ -14,4 +16,30 @@ import WithRender from './home.html';
 
 export default class Home extends Vue {
   public title: string = 'Mazes w/o Monsters!';
+
+  public mounted() {
+    window.addEventListener('keypress', this.handleKeypress);
+  }
+
+  public handleKeypress(event: any) {
+    let direction: string = '';
+    switch (event.key) {
+      case 'w':
+        direction = 'above';
+        break;
+      case 'a':
+        direction = 'left';
+        break;
+      case 's':
+        direction = 'below';
+        break;
+      case 'd':
+        direction = 'right';
+        break;
+    }
+    this.$props.maze.movePlayer(direction);
+    if (this.$props.maze.isGameOver()) {
+      alert('you win!');
+    }
+  }
 }
