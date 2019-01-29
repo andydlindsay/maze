0b9def92fb82e8e1df8471e18c9ab48411acca73
Andy Lindsay <andydlindsay@gmail.com>
2019-01-28 21:20:23 -0800

    adds express server and dist folder for Heroku deployment

---
+++ dist/precache-manifest.eb0a25f884345848f989079ec49d4283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b1a78bdcee38b2fd1ec3",
+    "url": "/js/chunk-vendors.af2524d4.js"
+  },
+  {
+    "revision": "b043223716dd97cd668d",
+    "url": "/js/app.d859fa58.js"
+  },
+  {
+    "revision": "b037295e325f538aa4c81bd5c9a11f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b043223716dd97cd668d",
+    "url": "/css/app.d7ca96b2.css"
+  }
+];
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(serveStatic(__dirname + "/dist"));
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
