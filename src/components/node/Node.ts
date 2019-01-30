0ffb24779fb0f42e0752c2eef3938376835fca38
Andy Lindsay <andydlindsay@gmail.com>
2019-01-30 00:08:24 -0800

    refactors Node component to move method logic directly into element class assignment

--- src/components/node/Node.ts
+++ src/components/node/Node.ts
@@ -7,35 +7,10 @@ import WithRender from './node.html';
     node: Object,
   },
   methods: {
-    hasAboveBorder() {
-      if (this.$props.node.y === 0) {
-        return true;
-      }
-    },
-    hasLeftBorder() {
-      if (this.$props.node.x === 0) {
-        return true;
-      }
-    },
-    hasRightBorder() {
-      return !this.$props.node.right;
-    },
-    hasBelowBorder() {
-      return !this.$props.node.below;
-    },
     needsCorner() {
       if (this.$props.node.right && this.$props.node.below) {
         return true;
       }
-    },
-    hasPlayer() {
-      return this.$props.node.player;
-    },
-    hasExit() {
-      return this.$props.node.exit;
-    },
-    beenVisited() {
-      return this.$props.node.visited;
     },
   },
 })
